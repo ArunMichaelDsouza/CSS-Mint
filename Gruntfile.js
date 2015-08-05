9fc1edf85a9a48c122400caf8875f62a942c6e06
Arun Michael Dsouza <amdsouza92@gmail.com>
2015-08-06 01:43:09 +0530

    Updating gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,18 @@ module.exports = function(grunt) {
                     'build/css-mint.min.css': ['src/css/normalize.css', 'src/css/buttons.css', 'src/css/footer.css', 'src/css/header.css', 'src/css/navbar.css', 'src/css/panel.css', 'src/css/typography.css', 'src/css/utilities.css']
                 }
             }
-        }
+        },
+        watch: {
+            scripts: {
+                files: ['src/css/*.css'],
+                tasks: ['cssmin'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
     });
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
 };
