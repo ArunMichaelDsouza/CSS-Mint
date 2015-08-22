b9ba93157c1b783ab88e4f686cac1fed71a16103
Arun Michael Dsouza <amdsouza92@gmail.com>
2015-08-23 01:03:41 +0530

    Updated button styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
         cssmin: {
             target: {
                 files: {
-                    'build/css-mint.min.css': ['src/css/normalize.css', 'src/css/buttons.css', 'src/css/footer.css', 'src/css/header.css', 'src/css/navbar.css', 'src/css/panel.css', 'src/css/typography.css', 'src/css/utilities.css', 'src/css/grid.css', 'src/css/card.css', 'src/css/base.css']
+                    'build/css-mint.min.css': ['src/css/normalize.css', 'src/css/buttons.css', 'src/css/footer.css', 'src/css/header.css', 'src/css/navbar.css', 'src/css/panel.css', 'src/css/typography.css', 'src/css/utilities.css', 'src/css/grid.css', 'src/css/card.css', 'src/css/base.css', 'src/css/alert.css']
                 }
             }
         },
