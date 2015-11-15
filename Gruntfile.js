687b8afa5bc0167ad3b23f3e677c173877bffd19
Arun Michael Dsouza <amdsouza92@gmail.com>
2015-11-15 16:36:02 +0530

    Box sizing resets for all elements

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,24 @@ module.exports = function(grunt) {
                 }
             }
         },
+        concat: {
+            dist: {
+                src: ['src/css/normalize.css', 'src/css/buttons.css', 'src/css/footer.css', 'src/css/header.css', 'src/css/navbar.css', 'src/css/panel.css', 'src/css/typography.css', 'src/css/utilities.css', 'src/css/grid.css', 'src/css/card.css', 'src/css/base.css', 'src/css/alert.css', 'src/css/input.css', 'src/css/tables.css', 'src/css/breadcrumb.css', 'src/css/pagination.css', 'src/css/badge.css', 'src/css/thumbnails.css'],
+                dest: 'build/css-mint.css',
+            },
+        },
         watch: {
             scripts: {
                 files: ['src/css/*.css'],
-                tasks: ['cssmin'],
+                tasks: ['concat','cssmin'],
                 options: {
                     spawn: false,
                 },
             },
-        },
+        }
     });
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.registerTask('default', ['watch']);
 };
