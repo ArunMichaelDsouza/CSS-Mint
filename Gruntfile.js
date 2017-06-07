580b1c26faba60889f76363ec509ae8be13d1ee8
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-06-07 17:56:20 +0530

    Sass build pipeline with grunt added

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,21 @@ module.exports = function(grunt) {
         cssmin: {
             target: {
                 files: {
-                    'build/css-mint.min.css': ['src/css/normalize.css', 'src/css/buttons.css', 'src/css/footer.css', 'src/css/header.css', 'src/css/navbar.css', 'src/css/panel.css', 'src/css/typography.css', 'src/css/utilities.css', 'src/css/grid.css', 'src/css/card.css', 'src/css/base.css', 'src/css/alert.css', 'src/css/input.css', 'src/css/tables.css', 'src/css/breadcrumb.css', 'src/css/pagination.css', 'src/css/badge.css', 'src/css/thumbnails.css']
+                    'build/css-mint.min.css': ['build/css-mint.css']
                 }
             }
         },
-        concat: {
+        sass: {
             dist: {
-                src: ['src/css/normalize.css', 'src/css/buttons.css', 'src/css/footer.css', 'src/css/header.css', 'src/css/navbar.css', 'src/css/panel.css', 'src/css/typography.css', 'src/css/utilities.css', 'src/css/grid.css', 'src/css/card.css', 'src/css/base.css', 'src/css/alert.css', 'src/css/input.css', 'src/css/tables.css', 'src/css/breadcrumb.css', 'src/css/pagination.css', 'src/css/badge.css', 'src/css/thumbnails.css'],
-                dest: 'build/css-mint.css',
-            },
+                files: {
+                    'build/css-mint.css': 'src/sass/main.scss'
+                }
+            }
         },
         watch: {
             scripts: {
-                files: ['src/css/*.css'],
-                tasks: ['concat','cssmin'],
+                files: ['src/sass/*.scss'],
+                tasks: ['sass', 'cssmin'],
                 options: {
                     spawn: false,
                 },
@@ -25,6 +26,6 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.registerTask('default', ['watch']);
 };
